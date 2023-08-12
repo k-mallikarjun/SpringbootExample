cd576fa53bd18a7bc7ef7f73753d2a9968209485
k-mallikarjun <k.mallikarjun113@gmail.com>
2023-08-12 13:09:31 +0530

    1stcommit

---
+++ src/test/java/SpringBootTraining/Example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootTraining.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
